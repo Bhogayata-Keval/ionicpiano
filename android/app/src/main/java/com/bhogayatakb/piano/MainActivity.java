6c6c8ed2cda9b916ba7e09081e51eee3e43617aa
Keval <keval@acquire.io>
2021-09-04 18:47:21 +0530

    testing firebase workflow

--- android/app/src/main/java/com/bhogayatakb/piano/MainActivity.java
+++ android/app/src/main/java/com/bhogayatakb/piano/MainActivity.java
@@ -1,5 +1,20 @@
 package com.bhogayatakb.piano;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+            add(GoogleAuth.class);
+        }});
+    }
+}
